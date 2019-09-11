27872f02b86888d9c54ab53385712150baf89640
Antonio Stanislaw <stanislaw.iaw2018@gmail.com>
2019-09-11 19:30:31 -0300

    Modificado o arquivo admin.py

--- despesa/app/admin.py
+++ despesa/app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+class DespesaAdmin(admin.ModelAdmin):
+    list= display = ('Data_criacao','forma_pagamento','descricao',
+                     'vencimento', 'quitacao')
+
+admin.site.register(Despesa, DespesaAdmin)
+
+
